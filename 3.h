c4c157c65b707a5d722d043daded894cc43f564a
Kirsaxg1 <dooe_x@vk.com>
2025-03-22 15:44:41 +0300

    sigma

---
+++ 3.h
@@ -0,0 +1,38 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+#include <iostream>
+
+class complex {
+
+	double actual_part;
+	double imaginary_part;
+
+	complex(const complex& other) = default;
+	~complex() = default;
+	complex(double actual_part = 0,double imaginary_part = 0);
+	complex& operator = (const complex& other) = default;
+
+	complex operator + (const complex& other) const;
+	complex& operator +=(const complex& other);
+
+	complex operator - (const complex& other) const;
+	complex& operator -= (const complex& other);
+
+	complex operator * (const complex& other) const;
+	complex& operator *= (const complex& other);
+
+	complex operator / (const complex& other) const;
+	complex& operator /= (const complex& other);
+
+	[[nodiscard]] double abs()const;
+	[[nodescard]] double arg()const;
+
+
+	friend std::ostream& operator<<(std::ostream& out, const complex& number);
+	friend std::istream& operator>>(std::istream& in, complex& number);
+
+};
+
+
+#endif // !COMPLEX_H
