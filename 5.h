7564fd06666d3f6494742178bff29fd4b6458f1d
Kirsaxg1 <dooe_x@vk.com>
2025-03-22 16:03:56 +0300

    sigma

--- 5.h
+++ 5.h
@@ -1,18 +1,34 @@
 #include <iostream>
 
-typedef struct PriorItem {
-	int priority;
-	char* data;
-}PriorItem;
+//typedef struct PriorItem {
+//	int priority;
+//	char* data;
+//}PriorItem;
+//
+//typedef struct PriorQueue {
+//	PriorItem* items;
+//	size_t size;
+//	size_t capacity;
+//}PriorQueue;
+//
+//
+//PriorQueue* insert_value();
+//PriorQueue* search_max();
+//PriorQueue* removeMax();
+//PriorQueue* merge();
 
-typedef struct PriorQueue {
-	PriorItem* items;
-	size_t size;
-	size_t capacity;
-}PriorQueue;
+class priority_queue {
 
+public:
 
-PriorQueue* insert_value();
-PriorQueue* search_max();
-PriorQueue* removeMax();
-PriorQueue* merge();
+	priority_queue(); 
+	~priority_queue() = default;
+	priority_queue& operator = (const priority_queue& other) = default;
+	priority_queue(const priority_queue& other) = default;
+
+	[[nodiscard]]void insert(char* str, int priority);
+	[[nodiscard]] int search()const;
+	[[nodiscard]] void remove()const;
+	priority_queue* merge(priority_queue* other);
+
+};
